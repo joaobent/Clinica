192b6780e4851556724be6bab230c228f6d93eb8
joaobent <joaopedro0744@hotmail.com>
2025-02-05 19:43:32 -0400

    a

--- medico/src/app/page.js
+++ medico/src/app/page.js
@@ -12,7 +12,6 @@ export default function Home() {
       </h1>
       <p className={styles.para}>Na Clínica Vitalidade, nosso compromisso é oferecer um atendimento de qualidade para você e sua família.<br/> Contamos com uma equipe de profissionais altamente qualificados e estrutura moderna para garantir diagnósticos precisos e tratamentos eficazes.<br/> </p>
       <p className={styles.para2}>Agende sua consulta e tenha a tranquilidade de estar em boas mãos. Sua saúde é a nossa prioridade!</p>
-      <p>foiiiiii</p>
     </div>
   );
 }
